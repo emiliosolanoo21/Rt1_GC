1d7d65ce262ae3e19df7563028ab7eac2db32ba4
emiliosolanoo21 <85262580+emiliosolanoo21@users.noreply.github.com>
2023-09-18 16:09:14 -0600

    Muñeco faltante solo de ojos

--- Raytracer.py
+++ Raytracer.py
@@ -16,11 +16,11 @@ screen.set_alpha(None)
 raytracer = Raytracer(screen)
 raytracer.rtClearColor(0.25,0.45,0.95)
 
-coal = Material(diffuse=(0.25,0.25,0.25), spec = 256,  ks = 0.1)
-carrot = Material(diffuse=(0.93,0.57,0.13), spec = 50,  ks = 0.3)
+coal = Material(diffuse=(0.15,0.15,0.15), spec = 5, ks = 0.02)
+carrot = Material(diffuse=(0.93,0.57,0.13), spec = 5, ks = 0.02)
 snow = Material(diffuse=(0.91,0.93,0.90), spec = 5, ks = 0.02)
-stone = Material(diffuse=(0.60,0.60,0.60), spec = 20, ks = 0.01)
-sclera = Material(diffuse=(0.86,0.88,0.85), spec = 20, ks = 0.01)
+stone = Material(diffuse=(0.60,0.60,0.60), spec = 5, ks = 0.02)
+sclera = Material(diffuse=(0.86,0.88,0.85), spec = 5, ks = 0.02)
 
 #Nieve
 raytracer.scene.append(Sphere(position=(0,-1.2,-5), radius = 1.2, material = snow))
@@ -31,6 +31,15 @@ raytracer.scene.append(Sphere(position=(0,1.94,-5), radius = 0.7, material = sno
 raytracer.scene.append(Sphere(position=(0,-0.8,-4), radius = 0.35, material = coal))
 raytracer.scene.append(Sphere(position=(0,0.15,-4), radius = 0.25, material = coal))
 raytracer.scene.append(Sphere(position=(0,1,-4), radius = 0.15, material = coal))
+
+#Zanahoria
+raytracer.scene.append(Sphere(position=(0,1.75,-4), radius = 0.20, material = carrot))
+
+#Boca
+raytracer.scene.append(Sphere(position=(-0.35,1.54,-4.15), radius = 0.075, material = stone))
+raytracer.scene.append(Sphere(position=(-0.13,1.49,-4.15), radius = 0.075, material = stone))
+raytracer.scene.append(Sphere(position=(0.12,1.51,-4.15), radius = 0.075, material = stone))
+raytracer.scene.append(Sphere(position=(0.35,1.55,-4.15), radius = 0.075, material = stone))
 
 #iluminacion minima del ambiente
 raytracer.lights.append(AmbientLight(intensity=0.1))
